de9f172bb7127861070d502addb1d7da8048bff6
RaulCompaired <raulcn@outlook.es>
2026-05-23 13:25:21 -0600

    V5. Validaciones de edad, correcciones de CSS y redirección dinámica

--- JS/login.js
+++ JS/login.js
@@ -1,19 +1,24 @@
-function validarlogin(){
+function validarlogin(destino){
         var errores = [];
         var boletav = document.forms.datos.boleta.value;
         var resboleta = boletav.match(/^\d{10}$|^(PE|PP)\d{8}$/);
         marcarCampo("boleta", resboleta);
 
         var passwordv = document.forms.datos.password.value;
-        var respassword = passwordv.match(/^[A-Za-z0-9]*[A-Z]+[A-Za-z0-9]*[0-9]+[A-Za-z0-9]*[a-z]+[A-Za-z0-9]*$/);
+        var respassword = passwordv.match(/^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9\s]).{6,}$/);
         marcarCampo("password", respassword);
 
         if(resboleta == null)  errores.push("Boleta inválida");
-
         if(respassword == null) errores.push("Contraseña inválida");
 
-          if (errores.length > 0) alert("los datos son inválidos.");
-          else  alert("¡Inicio de sesión exitoso!");
+        if (errores.length > 0) {
+            // Si hay errores, solo mostramos la alerta y el código se detiene aquí. No avanza.
+            alert("Los datos son inválidos.");
+        } else {
+            // Si no hay errores, mostramos el éxito y redirigimos a la cuenta
+            alert("¡Inicio de sesión exitoso!");
+            window.location.href = destino; 
+        }
 
     }
     function marcarCampo(id, resultado) {
--- JS/registro.js
+++ JS/registro.js
@@ -9,7 +9,7 @@
         marcarCampo("curp", rescurp);
 
         var nombrev = document.forms.datos.nombre.value;
-        var resnombre = nombrev.match(/^[A-Z][a-z]+ [A-Z][a-z]+$/);
+        var resnombre = nombrev.match(/^[A-Z][a-z]+ [A-Z][a-z]+( |[A-Z a-z])*$/);
         marcarCampo("nombre", resnombre);
 
         var telv = document.forms.datos.telefono.value;
@@ -19,6 +19,20 @@
         var correov = document.forms.datos.correo.value;
         var rescorreo = correov.match(/^[A-Za-z0-9_.]+@alumno.ipn.mx$/);
         marcarCampo("correo", rescorreo);
+
+        var passwordv = document.forms.datos.password.value;
+        var respass = passwordv.match(/^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9\s]).{6,}$/);
+        marcarCampo("password", respass);
+
+        const nacim = document.getElementById("nacimiento").value;
+        const prom = document.getElementById("promedio").value;
+        const tel = document.getElementById("telefono").value;
+        const generoElemento = document.querySelector('input[name="genero"]:checked');
+        const genero = generoElemento ? generoElemento.id : "No especificado";
+        const pf = document.getElementById("procedenciafed").value;
+        let ep = document.getElementById("escuelap").value;
+        if (ep === "Otro") ep = document.getElementById("nombreesc").value;
+        
 
         if(resboleta == null)  errores.push("Boleta inválida");
         
@@ -30,10 +44,35 @@
         
         if(rescorreo == null) errores.push("Correo inválida");
 
-          if (errores.length > 0) alert("Algunos datos son inválidos.");
-          else  alert("¡Registro exitoso! Boleta: " + boletav + "\nNombre: " + nombrev + "\nTelefono: " + telv + "\nCorreo Instituacional: " + correov + "\nCURP: " + curpv );
+        if(respass == null) errores.push("Contraseña inválida");
 
+        const listaModal = document.getElementById("modalp");  
+
+         if (errores.length > 0) {
+            document.getElementById("modalp").innerHTML = "<li class='list-group-item list-group-item-danger'>Los datos ingresados son inválidos</li>";
+            var miModalErrores = new bootstrap.Modal(document.getElementById('modalform'));
+            miModalErrores.show();
+        } 
+        else {
+        document.getElementById("modalp").innerHTML = 
+            "<li class='list-group-item'>No. Boleta: " + boletav + "</li>" +
+            "<li class='list-group-item'>Nombre: " + nombrev + "</li>" +
+            "<li class='list-group-item'>Fecha de Nacimiento: " + nacim + "</li>" +
+            "<li class='list-group-item'>Género: " + genero + "</li>" +
+            "<li class='list-group-item'>CURP: " + curpv + "</li>" +
+            "<li class='list-group-item'>Entidad Federativa: " + pf + "</li>" +
+            "<li class='list-group-item'>Telefono: " + tel + "</li>" +
+            "<li class='list-group-item'>Escuela de Procedencia: " + ep + "</li>" +
+            "<li class='list-group-item'>Promedio: " + prom + "</li>" +
+            "<li class='list-group-item'>Correo: " + correov + "</li>" +
+            "<li class='list-group-item'>Contraseña: " + passwordv + "</li>";
+
+        var miModal = new bootstrap.Modal(document.getElementById('modalform'));
+        miModal.show();
     }
+           
+    }
+    
     function marcarCampo(id, resultado) {
     var campo = document.getElementById(id);
     if (resultado == null) {
@@ -47,11 +86,36 @@
 
 document.getElementById('escuelap').addEventListener('change', function() {
     var otroInput = document.getElementById('nombreesc');
-    if (this.value === '23') {
+    if (this.value === 'Otro') {
         otroInput.disabled = false;
         otroInput.focus();
     } else {
         otroInput.disabled = true;
         otroInput.value = ''; 
     }
+});
+
+
+
+//CALENDARIO
+//Bloqueamos el calendario para que no puedan exigir fecha futura y que tengan almenos 16 (pensando en los genios) y una maximo de 100 años
+window.addEventListener('DOMContentLoaded', function() {
+    var campoNacimiento = document.getElementById('nacimiento');
+    var hoy = new Date();
+    
+    //El minimo de edad donde ademas comprobamos que sea de dos digitos para que se adapte al formato de html
+    var fechaMax = new Date(hoy);
+    fechaMax.setFullYear(fechaMax.getFullYear() - 16);
+    var anioMax = fechaMax.getFullYear();
+    var mesMax = String(fechaMax.getMonth() + 1).padStart(2, '0');
+    var diaMax = String(fechaMax.getDate()).padStart(2, '0');
+    campoNacimiento.max = anioMax + "-" + mesMax + "-" + diaMax;
+
+    //Repetimos el mismo proceso pero para poner el maximo de edad en 100 años
+    var fechaMin = new Date(hoy);
+    fechaMin.setFullYear(fechaMin.getFullYear() - 100);
+    var anioMin = fechaMin.getFullYear();
+    var mesMin = String(fechaMin.getMonth() + 1).padStart(2, '0');
+    var diaMin = String(fechaMin.getDate()).padStart(2, '0');
+    campoNacimiento.min = anioMin + "-" + mesMin + "-" + diaMin;
 });
