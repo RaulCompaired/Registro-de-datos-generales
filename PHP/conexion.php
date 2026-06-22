61a8ea283b6eb5464d3415cd4877b83b0bc4ec48
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2026-06-22 11:29:24 -0600

    Ultimos cambios

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -5,6 +5,7 @@ $username = 'root';
 $password = '';
 
     $conexion= mysqli_connect($host,$username,$password,$dbname);
+    //Para que no se muestren los errores de mysql
     mysqli_report(MYSQLI_REPORT_OFF);
 
  ?>
